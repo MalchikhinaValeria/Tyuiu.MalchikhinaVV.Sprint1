49a091e52ecd355b22ef9a05c1fc39bc13144c29
Home <Home@LAPTOP-L43VJUUA>
2025-09-24 22:53:08 +0500

    Task2

---
+++ Tyuiu.MalchikhinaVV.Sprint1.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.MalchikhinaVV.Sprint1.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 50;
+
+            var res = ds.ConvertKmToM(x);
+
+            Assert.AreEqual(31.07520198881293, res);
+        }
+    }
+}
---
+++ Tyuiu.MalchikhinaVV.Sprint1.Task2.V1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MalchikhinaVV.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.MalchikhinaVV.Sprint1.Task2.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Мальчихина В. В. | ИИПб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Мальчихина Валерия Викторовна | ИИПб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя расстояние       *");
+            Console.WriteLine("* в километрах, переводит его в мили и печатает результат на экране.      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите расстояние в километрах: ");
+            
+            int x = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($" {x} км = " + string.Format("{0:F3}", ds.ConvertKmToM(x)) + " миль");
+
+            Console.ReadLine();
+        }
+    }
+}
