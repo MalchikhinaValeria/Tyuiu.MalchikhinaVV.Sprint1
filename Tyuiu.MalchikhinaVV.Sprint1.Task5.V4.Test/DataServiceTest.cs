e37333f82f873876dda75469f06666ef12bb6383
Home <Home@LAPTOP-L43VJUUA>
2025-09-28 19:55:41 +0500

    Task5

---
+++ Tyuiu.MalchikhinaVV.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int k = 13257;
+            int h = ds.SecondsToHours(k);
+            int wait = 3;
+
+            Assert.AreEqual(wait, h);
+        }
+    }
+}
---
+++ Tyuiu.MalchikhinaVV.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MalchikhinaVV.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.MalchikhinaVV.Sprint1.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Мальчихина В. В. | ИИПб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                             *");
+            Console.WriteLine("* Выполнил: Мальчихина Валерия Викторовна | ИИПб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные         *");
+            Console.WriteLine("* данные, вычисляет результат и печатает его на экране.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество секунд: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Если идет {k} секунда суток, значит прошло {ds.SecondsToHours(k)} полных часов.");
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
